b4050bc509722573096e199fd2ab626d3a499e07
Alys-7 <deprez.alais1@gmail.com>
2026-02-23 16:59:54 +0100

    création de la fonction afficherPokemon

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const rejouer = document.querySelector('#rejouer');
 const boxes = document.querySelectorAll('.box');
 const pokemons_captures = document.querySelector('.liste_pokemons_captures');
 
+const listePokemon = ['pikachu','tiplouf','singicram','roselia','tiplouf','boustiflor','pikachu','roselia','tournevol','boustiflor','singicram','tournevol']
 const listePokemonsTrouves = [false, false, false, false, false, false, false, false, false, false, false, false];
 
 function buissonClique(indiceBox, listePokemonsTrouves) {
-    if (!listePokemonsTrouves[indiceBuisson]) {
+    if (!listePokemonsTrouves[indiceBox]) {
         retournerBuisson(indiceBox);
+        afficherPokemon(indiceBox);
     }
 }
 
@@ -19,3 +21,13 @@ function retournerBuisson(indiceBox) {
 function ajouterPokeball(indiceBox) {
     boxes[indiceBox].querySelector('.pokeball').style.display = 'block';
 }
+
+function afficherPokemon(indiceBox) {
+    const pokemon = document.createElement("div");
+    pokemon.textContent = listePokemon[indiceBox];
+    boxes[indiceBox].appendChild(pokemon);
+}
+
+boxes.forEach((box,i) => {
+    box.addEventListener("click", function() {buissonClique(i,listePokemonsTrouves)});
+})
