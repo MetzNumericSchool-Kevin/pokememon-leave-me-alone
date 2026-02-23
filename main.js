800ff82ddbfa740d667fd39da135d3e565e4082e
Alys-7 <deprez.alais1@gmail.com>
2026-02-23 14:39:21 +0100

    définition des constantes utiles du DOM

---
+++ main.js
@@ -0,0 +1,5 @@
+const stat_nombre_de_coups = document.querySelector('#stat_nombre_de_coups');
+const stat_nombre_de_victoires = document.querySelector('#stat_nombre_de_victoires');
+const rejouer = document.querySelector('#rejouer');
+const boxes = document.querySelectorAll('.box');
+const pokemons_captures = document.querySelector('.liste_pokemons_captures');
