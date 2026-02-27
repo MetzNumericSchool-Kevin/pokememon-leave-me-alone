a6ce5f888aa70d04b516078d73be39fc10fa87d1
Alys-7 <deprez.alais1@gmail.com>
2026-02-27 16:38:42 +0100

    renommage des variables en camelCase

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const stat_nombre_de_coups = document.querySelector('#stat_nombre_de_coups');
-const stat_nombre_de_victoires = document.querySelector('#stat_nombre_de_victoires');
+const statNombreDeCoups = document.querySelector('#stat_nombre_de_coups');
+const statNombreDeVictoires = document.querySelector('#stat_nombre_de_victoires');
 const rejouer = document.querySelector('#rejouer');
 const boxes = document.querySelectorAll('.box');
-const pokemons_captures = document.querySelector('.liste_pokemons_captures');
+const pokemonsCaptures = document.querySelector('.liste_pokemons_captures');
 
 const listePokemon = ['pikachu','tiplouf','singicram','roselia','tiplouf','boustiflor','pikachu','roselia','tournevol','boustiflor','singicram','tournevol']
 const listePokemonsTrouves = [false, false, false, false, false, false, false, false, false, false, false, false];
