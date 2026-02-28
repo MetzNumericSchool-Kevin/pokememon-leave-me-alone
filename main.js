e8ad48f8b397b651aa5b1b5cef31101d43b80330
Alys-7 <deprez.alais1@gmail.com>
2026-02-28 17:16:49 +0100

    modification de la fonction buissonClique dans le cas où deux buissons sont retournés

--- main.js
+++ main.js
@@ -6,11 +6,38 @@ const pokemonsCaptures = document.querySelector('.liste_pokemons_captures');
 
 const listePokemon = ['pikachu','tiplouf','singicram','roselia','tiplouf','boustiflor','pikachu','roselia','tournevol','boustiflor','singicram','tournevol']
 const listePokemonsTrouves = [false, false, false, false, false, false, false, false, false, false, false, false];
+const listePokemonsRetournes = [];
 
 function buissonClique(indiceBox, listePokemonsTrouves) {
-    if (!listePokemonsTrouves[indiceBox]) {
+    if (!listePokemonsTrouves[indiceBox] && listePokemonsRetournes.length < 2) {
         retournerBuisson(indiceBox);
         afficherPokemon(indiceBox);
+        listePokemonsTrouves[indiceBox] = true;
+        listePokemonsRetournes.push([indiceBox,listePokemon[indiceBox]]);
+        if (listePokemonsRetournes.length == 2) {
+            if (listePokemonsRetournes[0][1] == listePokemonsRetournes[1][1]) {
+                setTimeout(() => {
+                    //cacherPokemon(listePokemonsRetournes[0][0]);
+                    //cacherPokemon(listePokemonsRetournes[1][0]);
+                    ajouterPokeball(listePokemonsRetournes[0][0]);
+                    ajouterPokeball(listePokemonsRetournes[1][0]);
+                    //capturerPokemon(listePokemonsRetournes[0][1]);
+                    listePokemonsRetournes.pop();
+                    listePokemonsRetournes.pop();
+                }, 2000);
+            } else {
+                setTimeout(() => {
+                    //cacherPokemon(listePokemonsRetournes[0][0]);
+                    //cacherPokemon(listePokemonsRetournes[1][0]);
+                    retournerBuisson(listePokemonsRetournes[0][0]);
+                    retournerBuisson(listePokemonsRetournes[1][0]);
+                    listePokemonsTrouves[listePokemonsRetournes[0][0]] = false;
+                    listePokemonsTrouves[listePokemonsRetournes[1][0]] = false;
+                    listePokemonsRetournes.pop();
+                    listePokemonsRetournes.pop();
+                }, 2000);
+            }
+        }
     }
 }
 
@@ -30,4 +57,4 @@ function afficherPokemon(indiceBox) {
 
 boxes.forEach((box,i) => {
     box.addEventListener("click", function() {buissonClique(i,listePokemonsTrouves)});
-})
+});
